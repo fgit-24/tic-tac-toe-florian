52351ca8e06c7a65b14eefdf15727769e9d16221
Florian Hass <f91hama@gmail.com>
2024-07-20 16:15:35 +0000

    Get user input, store it it in the variable position

--- run.py
+++ run.py
@@ -28,5 +28,30 @@ game_board = ['-', '-', '-',
 def current_state():
     for i in range(0, 9, 3):
         print(game_board[i] + '|' + game_board[i + 1] + '|' + game_board[i + 2])
-    print()        
+    print()
 
+current_state()
+
+
+valid_move = False
+while not valid_move:
+    # Get user input for position
+    postition = input("Enter a number between 1 and 9: ")
+
+    # Ensure the player inputs a valid number
+    if position not in [str(i) for i in range(1, 10)]:
+        print("Invalid input. Please choose a valid position from 1 to 9.")
+        continue
+
+    # -1 Because our list starts with the index of 0
+    position = int(position) - 1
+
+    # Check if the chosen position is available
+    if game_board[position] == "-":
+        valid_move = True
+    else:
+        print('You can\'t go there, go again!')
+
+    # Place the player's marker on the board
+    game_board[position] = player
+    # Display the updated board
